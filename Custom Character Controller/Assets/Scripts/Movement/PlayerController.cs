3a20fa504afe0073d20117be998a686c1872fd64
kimlongvo <hello@kimlongvo.com>
2024-02-15 17:46:23 -0800

    cleaned up player controller

--- Custom Character Controller/Assets/Scripts/Movement/PlayerController.cs
+++ Custom Character Controller/Assets/Scripts/Movement/PlayerController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private float _speed;
+    [SerializeField] private float speed;
     [SerializeField] private float jumpVelocity;
     [SerializeField] private float gravity;
     [SerializeField] private float rotateSpeed;
@@ -14,15 +14,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject playerCamera;
 
     private bool grounded;
-    private float x, y, z;
-    
-    public float speed
-    {
-        get
-        {
-            return _speed;
-        }
-    }
+    private float velocityY;
 
     void Update()
     {
@@ -32,32 +24,41 @@ public class PlayerController : MonoBehaviour
 
     void MovePlayer()
     {
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        float x = Input.GetAxis("Horizontal");
+        float y = playerCamera.transform.eulerAngles.y;
+        float z = Input.GetAxis("Vertical");
 
-        float cameraRotationY = playerCamera.transform.eulerAngles.y;
+        RotatePlayer(x, y, z);
+        Jump();
 
+        float input = Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(z, 2));
+
+        Vector3 inputDirection = Quaternion.Euler(0, y, 0) * new Vector3(x, 0, z).normalized * input;
+        Vector3 verticalSpeed = new Vector3(0, velocityY, 0);
+        Vector3 playerMove = inputDirection + verticalSpeed;
+
+        characterController.Move(playerMove * speed * Time.deltaTime);
+    }
+
+    void RotatePlayer(float x, float y, float z)
+    {
         if(x != 0 || z != 0)
         {
             float rotationY = Mathf.Atan2(x, z) * Mathf.Rad2Deg;
             
-            float targetAngle = rotationY += cameraRotationY;
+            float targetAngle = rotationY += y;
             float currentAngle = Mathf.LerpAngle(transform.eulerAngles.y, targetAngle, rotateSpeed * Time.deltaTime);
 
             transform.rotation = Quaternion.Euler(0, currentAngle, 0);
         }
+    }
 
+    void Jump()
+    {
         if(Input.GetKeyDown(KeyCode.Space) && grounded)
         {
-            y = jumpVelocity;
+            velocityY = jumpVelocity;
         }
-
-        float input = Mathf.Sqrt(Mathf.Pow(x, 2) + Mathf.Pow(z, 2));
-        Vector3 direction = new Vector3(x, 0, z);
-        Vector3 jump = new Vector3(0, y, 0);
-        Vector3 playerMove = Quaternion.Euler(0, cameraRotationY, 0) * direction.normalized * input + jump;
-
-        characterController.Move(playerMove * _speed * Time.deltaTime);
     }
 
     void OnTriggerExit(Collider collider)
@@ -72,7 +73,7 @@ public class PlayerController : MonoBehaviour
     {
         if(collider.gameObject.layer == 6)
         {
-            y = 0;
+            velocityY = 0;
             grounded = true;
         }
     }
@@ -81,7 +82,7 @@ public class PlayerController : MonoBehaviour
     {
         if(!grounded)
         {
-            y -= gravity * Time.deltaTime;
+            velocityY -= gravity * Time.deltaTime;
         }
     }
 }
